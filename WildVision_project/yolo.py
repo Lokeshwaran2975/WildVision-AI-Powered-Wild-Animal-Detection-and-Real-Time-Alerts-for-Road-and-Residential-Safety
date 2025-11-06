7bd9bfd21e6986151b7e9d86db451f693438d558
Lokeshwaran P <lokeshwaran2975@gmail.com>
2025-11-06 20:50:54 +0530

    Fix path formatting and update main function

--- WildVision_project/yolo.py
+++ WildVision_project/yolo.py
@@ -6,7 +6,7 @@ def main():
 
     # Train the model on GPU
     model.train(
-        data=r"D:\ANIMAL-DETECTION-main\ANIMAL-DETECTION-main\Wildvision Dataset\data.yaml",
+        data=r"\ANIMAL-DETECTION-main\ANIMAL-DETECTION-main\Wildvision Dataset\data.yaml",
         epochs=150,
         imgsz=416,
         batch=4,
@@ -19,10 +19,11 @@ def main():
 
     # Run prediction on test images
     results = model.predict(
-        source=r"D:\ANIMAL-DETECTION-main\ANIMAL-DETECTION-main\Wildvision Dataset\images\test",
+        source=r"\ANIMAL-DETECTION-main\ANIMAL-DETECTION-main\Wildvision Dataset\images\test",
         save=True,
         device=0
     )
 
 if _name_ == "_main_":
+
     main()
